0cdb32af53a0f80a7eada5ccc0cf6155a5e458d9
Your Name <you@example.com>
2022-12-23 18:13:37 +0600

    review in home page added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import useReview from '../../hooks/useReview';
+import HomeReview from '../HomeReview/HomeReview';
 
 const Home = () => {
+    const [reviews,setReviews] = useReview()
+    const needReviews = reviews.slice(0,3)
     return (
         <div>
-           <div className=' grid grid-cols-3'>
+           <div className='hero-section grid grid-cols-3'>
             <div className=' col-span-2 w-3/4 mx-auto flex justify-center items-center'>
                 <div>
                 <h2 className=' text-7xl font-bold my-5 text-green-700 font-serif'>We Give Quality</h2>
@@ -13,6 +17,14 @@ const Home = () => {
             </div>
             <div className='my-10 w-3/4 '>
                 <img className=' rounded-lg' src="hero.jpg" alt="Laptop" />
+            </div>
+           </div>
+           <div className='review-section w-3/4 mx-auto my-10'>
+            <p className='text-5xl text-center font-bold'>See Reviews</p>
+            <div className="reviewContainer grid grid-cols-3 gap-3 my-10">
+                {
+                    needReviews.map(review=><HomeReview key={review.id} review={review}></HomeReview>)
+                }
             </div>
            </div>
         </div>
---
+++ src/components/HomeReview/HomeReview.js
@@ -0,0 +1,48 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+const HomeReview = (props) => {
+    const {name,review,img,rating}=props.review
+    let designRating='';
+    if(rating==5){
+        designRating= <span> 
+            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>     
+        </span>
+    }
+    else if (rating==4){
+        designRating= <span> 
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>    
+    </span>
+    }
+    else{
+        designRating= <span> 
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>    
+    </span>
+    }
+    return (
+        <div className='grid grid-cols-3 bg-yellow-200 px-3 py-2 rounded-lg'>
+            <div className='grid grid-cols-1'>
+                <img className='mx-auto w-20 rounded-full border-2 border-red-600' src={img} alt="userImage" />
+                <p className=' text-center font-semibold text-lg'>{name}</p>
+                <p className=' text-center'>{designRating}</p>
+            </div>
+            <div className=' col-span-2'>
+                <p className=' text-justify'>
+                    {review}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default HomeReview;
