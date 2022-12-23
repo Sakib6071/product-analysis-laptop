cd261a4abc8c9fb66b08fe3f9960ad1f2137cd40
Your Name <you@example.com>
2022-12-23 22:13:15 +0600

    readme file edit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <p>Not Found....404</p>
+            <p className='text-center text-red-500 font-semibold text-3xl my-20'>Not Found....404</p>
         </div>
     );
 };
