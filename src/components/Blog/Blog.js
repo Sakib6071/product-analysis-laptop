4bddf2e1ce345322bb61d4443f83362e8c29b8bb
Your Name <you@example.com>
2022-12-23 21:40:26 +0600

    blog section added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <p>blog</p>
+        <div className='w-3/4 mx-auto'>
+            <p className='text-5xl text-center font-bold my-5'>Blog</p>
+            <div className='my-8'>
+                <div className='my-8'>
+                    <p className="question font-semibold text-2xl"> 1. What is context API?</p>
+                    <p className="answer text-xl">Answer: Basically it's avoid the props drilling, If need a data send one component to another component and parent component level is so high then we need to use so props for sending data. But context API resolve this issue. Using context API we can send a data directly one to another component</p>
+                </div>
+                <div className='my-5'>
+                    <p className="question font-semibold text-2xl"> 1. What is Semantic Tag?</p>
+                    <p className="answer text-xl">Answer: Basically semantic tag is very useful for both developer and browser. It's help to understand the section. Suppose in the header part will cover the header tag. So developer and browser can easily understand that it is header and for that reason it's SEO friendly.</p>
+                </div>
+                <div className='my-5'>
+                    <p className="question font-semibold text-2xl"> 1. What is the difference between Inline,Inline Block and Block Element?</p>
+                    <p className="answer text-xl">Answer: Inline element take space as it's need and it's not take full line, on the other hand inline block element also take needed space,but here you can set height width of the element and block element is totally opposite of inline element. It's take whole line. This is the basic difference of inline, inline-block and block element</p>
+                </div>
+            </div>
         </div>
     );
 };
